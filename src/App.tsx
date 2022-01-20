fe13a57513e64001e82657fd2b7be0e10df548e9
anshuman9999 <anshuman.pandey9999@gmail.com>
2022-01-20 12:31:03 +0530

    basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
+interface People {
+  name: string;
+  age: number;
+  url: string;
+  note?: string;
+}
+
 const App = () => {
-  return <div className="App">this is app</div>;
+  const [people, setPeople] = useState<People[]>([]);
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="container-heading">React Typescript Crash Course</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
